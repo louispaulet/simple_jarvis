4e31b21b474467c96cf2701ed5123a524e4d96af
louispaulet <louispaulet13@gmail.com>
2023-07-22 17:16:41 +0200

    rename and remove stream files

--- stream_audio_request.py
+++ stream_audio_request.py
@@ -8,7 +8,6 @@ from gtts import gTTS
 from langdetect import detect
 from tempfile import TemporaryFile
 import pygame
-import openai
 
 def get_voice_command(r, source, retries=3):
     for _ in range(retries):
@@ -39,51 +38,46 @@ def load_api_key(file_path):
         print(f"Failed to load API key: {e}")
         sys.exit(1)
 
-def get_chunk_text(text_chunk):
-  if text_chunk is None:
-    return None
 
-  try:
-    if text_chunk['choices'][0]['delta']['content'] is not None:
-      chunk_text = text_chunk['choices'][0]['delta']['content']
-      if isinstance(chunk_text, str):
-        return chunk_text
-      else:
-        return None
-
-  except KeyError as e:
-    return None
-
-
-def chat_with_gpt(prompt, api_key, model='gpt-3.5-turbo', max_tokens=500, retries=1):
+def chat_with_gpt(prompt, api_key, model='gpt-3.5-turbo', max_tokens=500, retries=3):
     if not prompt:
         print('Nothing to request, prompt is empty')
         return None
-    
-    
-    openai.api_key = api_key
-    
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[{'role': 'system', 'content': 'You are a helpful assistant.'},
-                     {'role': 'user', 'content': prompt}],
-        temperature=0,
-        max_tokens = max_tokens,
-        stream=True  # this time, we set stream=True
-    )
-    
-    curr_sentence = []
-    for chunk in response:
-        chunk_text = get_chunk_text(chunk)
-        if chunk_text is not None:
-            curr_sentence.append(chunk_text)
 
-            if '\n' in chunk_text:
-              complete_sentence = ''.join(curr_sentence)
-              curr_sentence = []
-              print(complete_sentence)
-              # problem here! - the .mp3 is being read while the next tries to be written!
-              #speak(complete_sentence)
+    api_endpoint = 'https://api.openai.com/v1/chat/completions'
+
+    # Create the payload for the API request
+    data = {
+        'model': model,
+        'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                     {'role': 'user', 'content': prompt}],
+        'max_tokens': max_tokens
+    }
+
+    # Set the headers including your API key
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    }
+
+    for _ in range(retries):
+        try:
+            # Send the request to the OpenAI API
+            response = requests.post(api_endpoint, json=data, headers=headers)
+            # Handle the response
+            if response.status_code == 200:
+                # Get the generated message from the API response
+                print(response.json())
+                messages = response.json()['choices'][0]['message']['content']
+                return messages
+            else:
+                print(f'Request failed with status code {response.status_code}')
+                continue
+        except Exception as e:
+            print(f"Failed to request chat completion: {e}")
+            continue
+
+    print(f"Failed to get chat completion after {retries} attempts")
     return None
 
 def speak(text):
@@ -101,9 +95,6 @@ def speak(text):
     pygame.mixer.music.stop()
 
 def main(api_key_file, model, max_tokens):
-    api_key = load_api_key(api_key_file)
-    messages = chat_with_gpt("donne moi la recette de la tarte aux pommes", api_key, model, max_tokens)
-    return None
     r = sr.Recognizer()
     with sr.Microphone() as source:
         api_key = load_api_key(api_key_file)
